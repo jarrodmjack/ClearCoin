f2e7c8e6661f1a746a167ca7aff645ddbf487e78
jarrodmjack <jarrodmjack@gmail.com>
2022-09-05 23:31:21 -0400

    fixed portfolio dropdown search functionality. Added CSS

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -1,36 +1,16 @@
 
-console.log('prtfol')
-
-// async function fetchCurrenciesForDropdownMenu(){
-
-//   const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&order=market_cap_desc&per_page=250&page=1&sparkline=false')
-//   const data = await res.json()
-
-//   console.log(data)
-
-// }
-// fetchCurrenciesForDropdownMenu()
-
-
-
-document.querySelector('#dropdownBtn').addEventListener('click', enableDropDown)
-
-document.querySelector('#myInput').addEventListener('keypress', filterDropdownMenuItems)
-
-
-function enableDropDown() {
+function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// FILTER FOR DROPDOWN MENU
-function filterDropdownMenuItems() {
-  let input, filter, ul, li, a, i;
+function filterFunction() {
+  var input, filter, ul, li, a, i;
   input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
-  let div = document.querySelector("#myDropdown");
+  div = document.getElementById("myDropdown");
   a = div.getElementsByTagName("a");
   for (i = 0; i < a.length; i++) {
-   let txtValue = a[i].textContent || a[i].innerText;
+    txtValue = a[i].textContent || a[i].innerText;
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
       a[i].style.display = "";
     } else {
@@ -40,15 +20,51 @@ function filterDropdownMenuItems() {
 }
 
 
-let dropdownListItems = document.querySelectorAll('.listItems')
 
-Array.from(dropdownListItems).forEach((element) => { //create an array from deleteBtn variable
-  element.addEventListener('click', selectItemFromDropdownMenu) //add a click event listener to each item
-})
 
-function selectItemFromDropdownMenu(){
-  console.log(this.innerText)
-}
+
+
+
+// document.querySelector('#dropdownBtn').addEventListener('click', enableDropDown)
+// document.querySelector('#myInput').addEventListener('keypress', filterDropdownMenuItems)
+
+
+// function enableDropDown() {
+//   document.getElementById("myDropdown").classList.toggle("show");
+// }
+
+// // // FILTER FOR DROPDOWN MENU
+// function filterDropdownMenuItems() {
+//   let input, filter, ul, li, a, i;
+//   input = document.getElementById("myInput");
+//   filter = input.value
+//   let div = document.querySelector("#myDropdown");
+//   a = div.getElementsByTagName("a");
+//   for (i = 0; i < a.length; i++) {
+//    let txtValue = a[i].textContent || a[i].innerText;
+//     if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//       a[i].style.display = "";
+//     } else {
+//       a[i].style.display = "none";
+//     }
+//   }
+// }
+
+
+
+
+
+// let dropdownListItems = document.querySelectorAll('.listItems')
+
+// Array.from(dropdownListItems).forEach((element) => { //create an array from deleteBtn variable
+//   element.addEventListener('click', selectItemFromDropdownMenu) //add a click event listener to each item
+// })
+
+// function selectItemFromDropdownMenu(){
+//   console.log(this.innerText)
+// }
+
+
 
 
 
